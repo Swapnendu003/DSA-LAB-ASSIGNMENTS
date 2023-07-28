e8b40d4a5a8b81048212944378e1b86a2c0f1a45
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-07-28 15:27:16 +0530

    Update ass1.c

--- ass1.c
+++ ass1.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 // Function prototypes
-void insertElement(int **arr, int *size, int element);
-void deleteElement(int **arr, int *size, int element);
+void insertElement(int **arr, int *size, int element, int position);
+void deleteElement(int **arr, int *size, int position);
 int linearSearch(int *arr, int size, int element);
 void displayArray(int *arr, int size);
 
 int main() {
     int *arr = NULL;
     int size = 0;
-    int choice, element;
+    int choice, element, position;
 
     printf("Enter the size of the array: ");
     scanf("%d", &size);
@@ -38,23 +38,25 @@ int main() {
             case 1:
                 printf("Enter the element to insert: ");
                 scanf("%d", &element);
-                insertElement(&arr, &size, element);
+                printf("Enter the position to insert: ");
+                scanf("%d", &position);
+                insertElement(&arr, &size, element, position);
                 break;
 
             case 2:
-                printf("Enter the element to delete: ");
-                scanf("%d", &element);
-                deleteElement(&arr, &size, element);
+                printf("Enter the position to delete: ");
+                scanf("%d", &position);
+                deleteElement(&arr, &size, position);
                 break;
 
             case 3:
                 printf("Enter the element to search: ");
                 scanf("%d", &element);
-                int position = linearSearch(arr, size, element);
-                if (position == -1) {
+                int foundPos = linearSearch(arr, size, element);
+                if (foundPos == -1) {
                     printf("Element not found in the array.\n");
                 } else {
-                    printf("Element found at index %d.\n", position);
+                    printf("Element found at index %d.\n", foundPos);
                 }
                 break;
 
@@ -77,30 +79,38 @@ int main() {
     return 0;
 }
 
-void insertElement(int **arr, int *size, int element) {
+void insertElement(int **arr, int *size, int element, int position) {
+    if (position < 0 || position > *size) {
+        printf("Invalid position. Insertion failed.\n");
+        return;
+    }
+
     (*size)++;
     *arr = (int*)realloc(*arr, (*size) * sizeof(int));
-    (*arr)[(*size) - 1] = element;
+
+    // Shift elements to the right to make space for the new element
+    for (int i = *size - 1; i > position; i--) {
+        (*arr)[i] = (*arr)[i - 1];
+    }
+
+    (*arr)[position] = element;
     printf("Element inserted successfully.\n");
 }
 
-void deleteElement(int **arr, int *size, int element) {
-    int found = 0;
-    for (int i = 0; i < *size; i++) {
-        if ((*arr)[i] == element) {
-            for (int j = i; j < (*size) - 1; j++) {
-                (*arr)[j] = (*arr)[j + 1];
-            }
-            (*size)--;
-            *arr = (int*)realloc(*arr, (*size) * sizeof(int));
-            found = 1;
-            printf("Element deleted successfully.\n");
-            break;
-        }
+void deleteElement(int **arr, int *size, int position) {
+    if (position < 0 || position >= *size) {
+        printf("Invalid position. Deletion failed.\n");
+        return;
     }
-    if (!found) {
-        printf("Element not found in the array.\n");
+
+    // Shift elements to the left to overwrite the deleted element
+    for (int i = position; i < *size - 1; i++) {
+        (*arr)[i] = (*arr)[i + 1];
     }
+
+    (*size)--;
+    *arr = (int*)realloc(*arr, (*size) * sizeof(int));
+    printf("Element deleted successfully.\n");
 }
 
 int linearSearch(int *arr, int size, int element) {
