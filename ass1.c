7752a6e22175216adad3c1fb4fb34caee5dea7cd
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-07-28 15:00:53 +0530

    Update ass1.c

--- ass1.c
+++ ass1.c
@@ -1,97 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function prototypes
+void insertElement(int **arr, int *size, int element);
+void deleteElement(int **arr, int *size, int element);
+int linearSearch(int *arr, int size, int element);
+void displayArray(int *arr, int size);
+
 int main() {
-    int *my_array = NULL; // Initialize the pointer to NULL
-    int array_size = 0;   // Initialize the size of the array
+    int *arr = NULL;
+    int size = 0;
+    int choice, element;
 
-    // Function to print the array
-    void printArray() 
-    {
-        printf("Array: ");
-        for (int i = 0; i < array_size; i++) {
-            printf("%d ", my_array[i]);
+    do {
+        printf("\n------- Menu -------\n");
+        printf("1. Insert element\n");
+        printf("2. Delete element\n");
+        printf("3. Linear search\n");
+        printf("4. Display array\n");
+        printf("5. Exit\n");
+        printf("---------------------\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the element to insert: ");
+                scanf("%d", &element);
+                insertElement(&arr, &size, element);
+                break;
+
+            case 2:
+                printf("Enter the element to delete: ");
+                scanf("%d", &element);
+                deleteElement(&arr, &size, element);
+                break;
+
+            case 3:
+                printf("Enter the element to search: ");
+                scanf("%d", &element);
+                int position = linearSearch(arr, size, element);
+                if (position == -1) {
+                    printf("Element not found in the array.\n");
+                } else {
+                    printf("Element found at index %d.\n", position);
+                }
+                break;
+
+            case 4:
+                displayArray(arr, size);
+                break;
+
+            case 5:
+                // Free dynamically allocated memory before exiting
+                free(arr);
+                printf("Exiting the program.\n");
+                break;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
         }
-        printf("\n");
-    }
 
-    // Function to insert an element into the array
-    void insertElement(int element, int position) {
-        // Check if position is valid
-        if (position < 0 || position > array_size) {
-            printf("Invalid position for insertion.\n");
-            return;
-        }
-
-        // Increase the size of the array
-        array_size++;
-        my_array = (int *)realloc(my_array, array_size * sizeof(int));
-
-        // Shift elements to the right to make space for the new element
-        for (int i = array_size - 1; i > position; i--) {
-            my_array[i] = my_array[i - 1];
-        }
-
-        // Insert the new element at the specified position
-        my_array[position] = element;
-        printf("Element %d inserted successfully.\n", element);
-    }
-
-    // Function to delete an element from the array
-    void deleteElement(int position) {
-        // Check if position is valid
-        if (position < 0 || position >= array_size) {
-            printf("Invalid position for deletion.\n");
-            return;
-        }
-
-        // Shift elements to the left to remove the element
-        for (int i = position; i < array_size - 1; i++) {
-            my_array[i] = my_array[i + 1];
-        }
-
-        // Decrease the size of the array
-        array_size--;
-        my_array = (int *)realloc(my_array, array_size * sizeof(int));
-        printf("Element at position %d deleted successfully.\n", position);
-    }
-
-    // Step 1: Get the size of the array from the user
-    printf("Enter the initial size of the array: ");
-    scanf("%d", &array_size);
-
-    // Step 2: Create a 1D array (dynamic memory allocation) with user input elements
-    my_array = (int *)malloc(array_size * sizeof(int));
-    printf("Enter %d elements of the array:\n", array_size);
-    for (int i = 0; i < array_size; i++) {
-        scanf("%d", &my_array[i]);
-    }
-
-    // Step 3: Print the initial array
-    printf("Initial ");
-    printArray();
-
-    // Step 4: Allow the user to insert elements
-    int element_to_insert, position_to_insert;
-    printf("Enter the element to insert: ");
-    scanf("%d", &element_to_insert);
-
-    printf("Enter the position to insert (0-indexed): ");
-    scanf("%d", &position_to_insert);
-
-    insertElement(element_to_insert, position_to_insert);
-    printArray();
-
-    // Step 5: Allow the user to delete elements
-    int position_to_delete;
-    printf("Enter the position to delete (0-indexed): ");
-    scanf("%d", &position_to_delete);
-
-    deleteElement(position_to_delete);
-    printArray();
-
-    // Step 6: Free dynamically allocated memory
-    free(my_array);
+    } while (choice != 5);
 
     return 0;
+}
+
+void insertElement(int **arr, int *size, int element) {
+    (*size)++;
+    *arr = (int*)realloc(*arr, (*size) * sizeof(int));
+    (*arr)[(*size) - 1] = element;
+    printf("Element inserted successfully.\n");
+}
+
+void deleteElement(int **arr, int *size, int element) {
+    int found = 0;
+    for (int i = 0; i < *size; i++) {
+        if ((*arr)[i] == element) {
+            for (int j = i; j < (*size) - 1; j++) {
+                (*arr)[j] = (*arr)[j + 1];
+            }
+            (*size)--;
+            *arr = (int*)realloc(*arr, (*size) * sizeof(int));
+            found = 1;
+            printf("Element deleted successfully.\n");
+            break;
+        }
+    }
+    if (!found) {
+        printf("Element not found in the array.\n");
+    }
+}
+
+int linearSearch(int *arr, int size, int element) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == element) {
+            return i;
+        }
+    }
+    return -1; // Element not found
+}
+
+void displayArray(int *arr, int size) {
+    printf("Array elements: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
